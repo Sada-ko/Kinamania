b3f467dbcbcb3e4a3c36a93d547c08668659d826
ANDREY_DESIGNER\Andrey <kaoru-san@yandex.ru>
2017-09-30 16:24:59 +0300

    Начерно сделал десктоп

--- html beta/js/app.js
+++ html beta/js/app.js
@@ -3,5 +3,26 @@
  */
 
 $(document).ready(function () {
-    $('.dropdown-toggle').dropdown();
-}
+
+    //Выпадающее меню
+    $('.dropdown-toggle').dropdown({
+        flip: false
+    });
+
+
+    //Owl Carousel
+    $(".videos-list").owlCarousel({
+        nav: true,
+        navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right ml-3' aria-hidden='true'></i>"],
+        autoWidth:true,
+        margin: 20
+    });
+
+    /*$("#scroll-menu").owlCarousel({
+        //nav: true,
+        navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right ml-3' aria-hidden='true'></i>"],
+        autoWidth:true,
+        margin: 0
+    });*/
+
+});
