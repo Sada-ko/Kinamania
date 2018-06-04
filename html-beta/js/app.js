6a3222d683546e4373ebf5c279148665b10142bf
ANDREY_DESIGNER\Andrey <kaoru-san@yandex.ru>
2018-06-04 17:05:21 +0300

    Финальные правки перед публикацией.

--- html-beta/js/app.js
+++ html-beta/js/app.js
@@ -1,17 +1,38 @@
 /**
  * Created by Sada-Mac on 24/09/17.
  */
+/*
+    Функция распарсит урлд видео и вирнет его ID
+*/
+function YouTubeGetID(url){
+  var ID = '';
+  url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
+  if(url[2] !== undefined) {
+    ID = url[2].split(/[^0-9a-z_\-]/i);
+    ID = ID[0];
+  }
+  else {
+    ID = url;
+  }
+    return ID;
+}
+jQuery(document).ready(function () {
+    // Обработка ссылки на видео
+    jQuery('.youtube_video_link').hide();
+    var youtube_link = jQuery('.youtube_video_link > span > a').text();
+    youtube_video_id = YouTubeGetID(youtube_link);
 
-$(document).ready(function () {
+
+
 
     //Выпадающее меню
-    $('.dropdown-toggle').dropdown({
+    jQuery('.dropdown-toggle').dropdown({
         flip: false
     });
 
 
     //Owl Carousel
-    $(".videos-list.owl-carousel").owlCarousel({
+    jQuery(".videos-list.owl-carousel").owlCarousel({
         nav: true,
         navText: ["<i class='fa fa-angle-left' aria-hidden='true'></i>","<i class='fa fa-angle-right ml-3' aria-hidden='true'></i>"],
         autoWidth:true,
@@ -38,6 +59,7 @@ $(document).ready(function () {
                 el.hide();
             });
         };
+        $('.fill-box').fillBox();
     }(jQuery, window, document));
 
 });
